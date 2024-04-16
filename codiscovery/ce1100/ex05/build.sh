1e5da1bb7757b13a8f002a66360f32b9f780376b
Nataliya Petrova <npetrova@made-f0Br8s14.clusters.42paris.fr>
2024-04-16 11:56:46 +0200

    first part

---
+++ codiscovery/ce1100/ex05/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#echo $1
+
+#mkdir ex$1
+
+
+#echo "Home of the current user is: $HOME"
+
+
+chifre=$1
+
+mkdir ex$1
+ls -la
+
+
+echo "ecrire mon de dossier:"
+read dirname
+mkdir ~/ex$dirname
